fdbdac0d8dd86fac4ed39909e8812da9f1c402a9
HARSHOP50 <XYZ9MC@GMAIL.COM>
2026-09-16 10:41:59 +0530

    THIS IS MY FIRST PROGRAM OF NO. ADD,SUB,MULM,DIV

---
+++ firstcode.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    double x,y; 
+    cout << "Enter the num1 : \n" ;
+    cin >> x;
+    cout << "Enter the num2 : \n" ;
+    cin >>  y ;
+    cout << "the sum is " << x + y << endl;
+    cout << "the sub is " << x - y << endl;
+    cout << "the multiplication is "<< x*y <<endl;
+    cout << "the division is " << x/y << endl;
+ }
